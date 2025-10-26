4527e014bbd44d1c073ec5c11e7e3da6a1e4e1cf
ella hain <ehain@bu.edu>
2025-10-26 15:45:34 -0400

    added Open AI API key to config

--- myApp/src/firebaseConfig.ts
+++ myApp/src/firebaseConfig.ts
@@ -14,9 +14,9 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  openAiApiKey: import.meta.env.VITE_OPEN_AI_API_KEY,
 };
-console.log("API key:", import.meta.env.VITE_FIREBASE_API_KEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
