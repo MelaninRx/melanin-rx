31749d3053b1b72343a50247d32264a7f3cf2247
yilan hu <lanhu@bu.edu>
2025-11-08 18:39:00 -0500

    adjust the UI layout

--- myApp/src/pages/Onboarding.tsx
+++ myApp/src/pages/Onboarding.tsx
@@ -4,25 +4,29 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  IonIcon
 } from "@ionic/react";
 import OnboardingForm from "../components/OnboardingForm";
+import "./Onboarding.css"
+import MelaninRxIcon from '../icons/MelaninRX.svg';
 
 const OnboardingPage: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      
+      <IonHeader className="onboarding-header">
         <IonToolbar>
-          <IonTitle>Welcome to MelaninRx 💜</IonTitle>
+          <div className="header-left">
+            <IonIcon icon={MelaninRxIcon} />
+          </div>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding ion-color-light">
-        <div style={{ 
-          background: '#ffffff', 
-          color: '#000000', 
-          minHeight: '100%',
-          padding: '20px'
-        }}>
-          <OnboardingForm />
+
+      <IonContent className="onboarding-bg">
+        <div className="onboarding-container">
+          <div className="onboarding-card"> 
+            <OnboardingForm />
+          </div>
         </div>
       </IonContent>
     </IonPage>
