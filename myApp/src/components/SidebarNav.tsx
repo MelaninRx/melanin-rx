9cdc513729f9e2a0b421c5f088134f86e79f6627
ella hain <ehain@bu.edu>
2025-11-21 02:26:27 -0500

    made sidebar component

---
+++ myApp/src/components/SidebarNav.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { IonButton, IonIcon } from '@ionic/react';
+import menuIcon from '../icons/menu.svg';
+import homeIcon from '../icons/house.svg';
+import addIcon from '../icons/Vector.svg';
+import chatbotIcon from '../icons/message-square.svg';
+import communityIcon from '../icons/users.svg';
+import timelineIcon from '../icons/calendar-days.svg';
+import AppointmentIcon from '../icons/Frame 112.svg';
+import settingsIcon from '../icons/settings.svg';
+import './SidebarNav.css';
+
+const SidebarNav: React.FC = () => (
+  <aside className="side-panel">
+    <div className="nav-top">
+      <IonButton fill="clear" routerLink="/menu">
+        <IonIcon icon={menuIcon} />
+        <span className="menu-text">Menu</span>
+      </IonButton>
+      <IonButton fill="clear" routerLink="/home">
+        <IonIcon icon={homeIcon} />
+        <span className="menu-text">Home</span>
+      </IonButton>
+      <IonButton fill="clear" routerLink="/add">
+        <IonIcon icon={addIcon} />
+        <span className="menu-text">New Chat</span>
+      </IonButton>
+      <IonButton fill="clear" routerLink="/chatbot">
+        <IonIcon icon={chatbotIcon} />
+        <span className="menu-text">Chats</span>
+      </IonButton>
+      <IonButton fill="clear" routerLink="/community">
+        <IonIcon icon={communityIcon} />
+        <span className="menu-text">Communities</span>
+      </IonButton>
+      <IonButton fill="clear" routerLink="/timeline">
+        <IonIcon icon={timelineIcon} />
+        <span className="menu-text">Timeline</span>
+      </IonButton>
+      <IonButton fill="clear" routerLink="/appointments">
+        <IonIcon icon={AppointmentIcon} />
+        <span className="menu-text">Appointments</span>
+      </IonButton>
+    </div>
+    <div className="nav-bottom">
+      <IonButton fill="clear" routerLink="/settings">
+        <IonIcon icon={settingsIcon} />
+        <span className="menu-text">Setting</span>
+      </IonButton>
+    </div>
+  </aside>
+);
+
+export default SidebarNav;
